50ffc27180ac924f2b93e43f2932acf153f25e06
Galymzhan K <capomafiosoken@gmail.com>
2020-04-12 15:57:25 +0600

    auth disabled

--- routes/api.php
+++ routes/api.php
@@ -21,22 +21,37 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', 'Api\PassportController@login');
 Route::post('register', 'Api\PassportController@register');
 
-Route::middleware(['auth:api', 'can:isAdmin'])->group(function (){
-    Route::apiResources([
-        'products'=>'Api\ProductController',
-        'users' => 'Api\UserController',
-        'addresses' => 'Api\AddressController',
-        'brands' =>'Api\BrandController',
-        'categories' =>'Api\CategoryController',
-        'cities' => 'Api\CityController',
-        'currencies' => 'Api\CurrencyController',
-        'filterGroups' => 'Api\FilterGroupController',
-        'filterValues' => 'Api\FilterValueController',
-        'orders' => 'Api\OrderController',
-        'productImages' => 'Api\ProductImageController',
-        'regions' => 'Api\RegionController',
-        'roles' => 'Api\RoleController'
-    ]);
-});
+//Route::middleware(['auth:api', 'can:isAdmin'])->group(function (){
+//    Route::apiResources([
+//        'products'=>'Api\ProductController',
+//        'users' => 'Api\UserController',
+//        'addresses' => 'Api\AddressController',
+//        'brands' =>'Api\BrandController',
+//        'categories' =>'Api\CategoryController',
+//        'cities' => 'Api\CityController',
+//        'currencies' => 'Api\CurrencyController',
+//        'filterGroups' => 'Api\FilterGroupController',
+//        'filterValues' => 'Api\FilterValueController',
+//        'orders' => 'Api\OrderController',
+//        'productImages' => 'Api\ProductImageController',
+//        'regions' => 'Api\RegionController',
+//        'roles' => 'Api\RoleController'
+//    ]);
+//});
 
+Route::apiResources([
+    'products'=>'Api\ProductController',
+    'users' => 'Api\UserController',
+    'addresses' => 'Api\AddressController',
+    'brands' =>'Api\BrandController',
+    'categories' =>'Api\CategoryController',
+    'cities' => 'Api\CityController',
+    'currencies' => 'Api\CurrencyController',
+    'filterGroups' => 'Api\FilterGroupController',
+    'filterValues' => 'Api\FilterValueController',
+    'orders' => 'Api\OrderController',
+    'productImages' => 'Api\ProductImageController',
+    'regions' => 'Api\RegionController',
+    'roles' => 'Api\RoleController'
+]);
 
