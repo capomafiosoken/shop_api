48978cf7c24e332411b6abfcc2baae04c2a93741
aibekzh <aibek_zhumash@mail.ru>
2020-04-12 15:43:14 +0600

    seeds added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -73,6 +73,176 @@ class DatabaseSeeder extends Seeder
                 'name'=>'Атырау'
             ],
         ]);
+        DB::table('addresses')->insert([
+           [
+              'city_id'=>1,
+              'zip_code'=>'6666',
+              'address'=>'Дом Ерсына',
+               'full_name'=>'Зачем нам это поле?',
+               'telephone_number'=>'87021234567',
+               'note'=>'ывфывфывфывфыввфыв ыфввфывф фывфы'
+           ] ,
+            [
+                'city_id'=>2,
+                'zip_code'=>'6666',
+                'address'=>'Дом Азамата',
+                'full_name'=>'Зачем нам это поле?',
+                'telephone_number'=>'87021234567',
+                'note'=>'ывфывфывфывфыввфыв ыфввфывф фывфы'
+            ] ,
+            [
+                'city_id'=>3,
+                'zip_code'=>'6666',
+                'address'=>'Дом Галымжана',
+                'full_name'=>'Зачем нам это поле?',
+                'telephone_number'=>'87021234567',
+                'note'=>'ывфывфывфывфыввфыв ыфввфывф фывфы'
+            ] ,
+        ]);
+        DB::table('brands')->insert([
+            [
+                'name'=>'louis vuitton',
+                'alias'=>'LV',
+                'description'=>'ОписаниеLouis Vuitton — французский дом моды,
+                специализирующийся на производстве чемоданов и сумок, модной одежды,
+                парфюмерии и аксессуаров класса «люкс» под одноимённой торговой маркой.',
+                //'image'=>'',
+
+            ],
+            [
+                'name'=>'Supreme',
+                'alias'=>'Supreme',
+                'description'=>'Supreme — американский стритвер-бренд одежды, основанный в Нью-Йорке в апреле 1994 года',
+                //'image'=>'',
+
+            ],
+            [
+                'name'=>'hewlett packard',
+                'alias'=>'HP',
+                'description'=>'Hewlett-Packard — одна из крупнейших американских компаний в сфере информационных
+                технологий, существовавшая в период 1939—2015 годов',
+                //'image'=>'',
+
+            ]
+        ]);
+        DB::table('categories')->insert([
+            [
+                'name'=>'Техника и электроника',
+                'alias'=>'электротехника',
+                'parent_id'=>null,
+                'keyword'=>'электротехника',
+                //'description'=>'',
+                //'image'=>'image',
+            ],
+            [
+                'name'=>'Ноутбуки и нетбуки',
+                'alias'=>'ноуты',
+                'parent_id'=>'1',
+                'keyword'=>'ноуты',
+                //'description'=>'',
+                //'image'=>'image',
+            ],
+            [
+                'name'=>'Одежда и обувь',
+                'alias'=>'одежда',
+                'parent_id'=>null,
+                'keyword'=>'шмот',
+                //'description'=>'',
+                //'image'=>'image',
+            ],
+        ]);
+        DB::table('currencies')->insert([
+            [
+                'name'=>'Тенге',
+                'code'=>'KZT',
+                'value'=>1,
+                'base'=>'0',
+
+            ]
+        ]);
+        DB::table('orders')->insert([
+            [
+              'user_id'=>1,
+              'status'=>'0',
+              'currency_id'=>1,
+              'address_id'=>1,
+
+            ],
+            [
+                'user_id'=>1,
+                'status'=>'0',
+                'currency_id'=>1,
+                'address_id'=>2,
+
+            ],
+            [
+                'user_id'=>1,
+                'status'=>'0',
+                'currency_id'=>1,
+                'address_id'=>3,
+
+            ],
+        ]);
+        DB::table('products')->insert([
+            [
+                'status'=>'0',
+                'name'=>'Ноутбук HP 2RR85EA ProBook',
+                'alias'=>'HP-2RR85EA',
+                'brand_id'=>3,
+                'price'=>441590,
+
+            ],
+            [
+                'status'=>'0',
+                'name'=>'КОФТА ONTHEGO GM',
+                'alias'=>'ONTHEGO-GM',
+                'brand_id'=>1,
+                'price'=>800000,
+
+            ],
+            [
+                'status'=>'0',
+                'name'=>'Футболка Supreme',
+                'alias'=>'shirt-supreme',
+                'brand_id'=>2,
+                'price'=>14490,
+
+            ],
+        ]);
+        DB::table('category_product')->insert([
+            [
+                'category_id'=>2,
+                'product_id'=>1,
+            ],
+            [
+                'category_id'=>3,
+                'product_id'=>2,
+            ],
+            [
+                'category_id'=>3,
+                'product_id'=>3,
+            ],
+        ]);
+        DB::table('order_product')->insert([
+            [
+                'product_id'=>1,
+                'order_id'=>1,
+                'pieces'=>1,
+                'price'=>441590,
+            ],
+            [
+                'product_id'=>2,
+                'order_id'=>2,
+                'pieces'=>1,
+                'price'=>800000,
+            ],
+            [
+                'product_id'=>3,
+                'order_id'=>3,
+                'pieces'=>1,
+                'price'=>14490,
+            ],
+        ]);
         // $this->call(UserSeeder::class);
     }
 }
